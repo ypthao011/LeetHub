68b927cf7fbfa962ceeb7d93b478da83c3d4221a
ypthao011 <80505205+ypthao011@users.noreply.github.com>
2024-02-10 00:09:10 -0600

    Time: 1 ms, Memory: 45.2 MB - LeetHub

---
+++ find-pivot-index/find-pivot-index.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        //add all numbers in list
+        //have a marker that moves right
+        //while marker < total, move right, decrement from total
+        //if marker = total return postion
+        //if marker > total break return -1
+        //if total = 0 return 0
+        int rightSum = 0;
+        //finds sum of list, also a placeholder for right side pivot
+        for(int i: nums){
+            rightSum += i;
+        }
+        
+        //start conditions
+        int leftSum = nums[0];
+        rightSum -= nums[0];
+        if(rightSum == 0){
+            return 0;
+        }
+        rightSum -= nums[1];
+        //j start at position 1 because left always start with [0]
+        for(int j = 1; j< nums.length-1; j++){
+            //check condition for rightsum pivot            
+            if(leftSum == rightSum){
+                return j;
+            }
+            
+            leftSum += nums[j];
+            rightSum -= nums[j+1];
+            
+            if(leftSum == rightSum){
+                return j+1;
+            }
+        }
+        
+        leftSum += nums[nums.length-1];
+        rightSum -= nums[nums.length-1];
+        if(leftSum == rightSum ){
+            return nums.length-1;
+        }
+
+         return -1;
+    }
+}
